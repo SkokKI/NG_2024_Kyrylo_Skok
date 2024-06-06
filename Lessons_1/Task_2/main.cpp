13ab3df91f13d06e7fa21911039a7c8af9264ca1
SkokKI <skok239@ukr.net>
2024-06-07 02:18:14 +0300

    Added second task

---
+++ Lessons_1/Task_2/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int first_num, second_num, result;
+    cout << "Input first number\n";
+    cin>> first_num;
+    cout << "Input second number\n";
+    cin>> second_num;
+    result = first_num+second_num;
+    cout<<result;
+    return 0;
+}
