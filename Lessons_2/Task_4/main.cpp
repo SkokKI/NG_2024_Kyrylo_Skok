ee928b29e0f222443dd80e88b36599740f27f60b
SkokKI <skok239@ukr.net>
2024-06-17 19:25:49 +0300

    Reworked task 4 of lesson 2

--- Lessons_2/Task_4/main.cpp
+++ Lessons_2/Task_4/main.cpp
@@ -5,19 +5,25 @@ using namespace std;
 int main()
 {
     char sentence[256] = "";
-    int words_counter = 1;
+    int words_counter = 0;
+    int words_num = 0;
     char first_letter = ' ';
     char second_letter = ' ';
-    char space = ' ';
     cout << "Input your sentences" << endl;
     cin.getline(sentence, 256);
     for(int letters_counter = 0; letters_counter < sizeof(sentence); letters_counter++){
         first_letter = sentence[letters_counter];
         second_letter = sentence[letters_counter + 1];
-        if(first_letter == space && second_letter != space){
+        if(first_letter >= 'a' && first_letter <= 'z' || first_letter >= 'A' && first_letter <= 'Z' ){
+            words_counter = 0;
+        }
+        else{
             words_counter++;
+            if(words_counter == 1){
+                words_num++;
+            }
         }
     }
-    cout << words_counter;
+    cout << words_num;
     return 0;
 }
