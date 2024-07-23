a3870037b9a73acd3574680129a810967acac6f3
SkokKI <skok239@ukr.net>
2024-07-23 09:11:33 +0300

    Reworked task 3 of 1 lesson

--- Lessons_1/Task_3/main.cpp
+++ Lessons_1/Task_3/main.cpp
@@ -7,16 +7,18 @@ int main()
     int salary = 0;
     cout << "How much do you earn?" << endl;
     cin >> salary;
+    if(salary > 999){
+        if(salary > 1000000){
+            cout << "Yep, you are milionarie";
+            return 0;
+        }
+        cout << "Well working";
+        return 0;
+    }
     if(salary < 1000){
         cout << "Work more";
         return 0;
     }
-    if(salary > 1000000){
-        cout << "Yep, you are milionarie";
-        return 0;
-    }
-    if(salary > 999){
-        cout << "Well working";
-        return 0;
-    }
+    cout << "Well working";
+    return 0;
 }
