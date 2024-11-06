a1744ca1aaa43669e874ebbfa3907f8744497777
SkokKI <skok239@ukr.net>
2024-11-07 01:43:50 +0200

    Reworked task 5 of 2 lesson, symbol translate in upper registr

--- Lesson_2/Task_5/main.cpp
+++ Lesson_2/Task_5/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <cctype>
 
 using namespace std;
 
@@ -8,15 +7,17 @@ int main()
     string user_line;
     int num_word = 0;
     char prev_letter = ' ';
-    char curr_letter = ' ';
     cout << "Input your line" << endl;
     getline(cin, user_line);
     for(int symb_counter = 0; symb_counter < user_line.length(); symb_counter++){
-        curr_letter = toupper(user_line[symb_counter]);
-        if(isalpha(curr_letter) == true && isalpha(prev_letter) != true){
-            num_word++;
+        if(toupper(user_line[symb_counter]) >= 'A' && toupper(user_line[symb_counter]) <= 'Z'){
+            if(toupper(prev_letter) >= 'A' && toupper(prev_letter) <= 'Z'){
+            }
+            else{
+                num_word++;
+            }
         }
-            prev_letter = curr_letter;
+            prev_letter = user_line[symb_counter];
     }
     cout << "Number of words in your line: " << num_word << endl;
     return 0;
